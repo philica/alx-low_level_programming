7dc5e58a9b6f4eca595c073e0de2db39bb44fc03
philica <fikretucker79@gmail.com>
2021-06-22 06:12:11 -0700

    create 0x02-function with some dir inside

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
-#include "holberton.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+int _putchar(char c)
+{
+return (write(1,&c,1));
+}
+
+char new_line()
+{
+return (write(1,"\n",1));
+}
 int main(void)
 {
 _putchar('H');
