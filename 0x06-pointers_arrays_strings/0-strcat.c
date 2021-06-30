84665310c1783201f2b897af1d1cd68f6f27f522
philica <fikretucker79@gmail.com>
2021-06-30 03:33:53 -0700

    create mock of strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,11 @@
+#include "holberton.h"
+/**
+ *  * _strcat - concatenates two strings
+ *   * @dest: input value
+ *    * @src: input value
+ *     *
+ *      * Return: void
+ *       */
 char *_strcat(char *dest, char *src)
 {
 	int i=0,j=0;
