6b1eafecd0d3b6eca481366411f5d8974c3e4217
philica <fikretucker79@gmail.com>
2021-06-30 03:30:42 -0700

    create mock of strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+char *_strcat(char *dest, char *src)
+{
+	int i=0,j=0;
+	while(dest[i]!="\0")
+	{
+		i++;
+	}
+
+	while(src[j]="\0")
+	{
+		dest[i]=src[j];
+		i++;
+		j++;
+	}
+	dest[j]="\0";
+	return (dest);
+}
