4f4e9e543942d96e979aeb78705dca5070bbf11d
philica <fikretucker79@gmail.com>
2021-07-01 00:19:41 -0700

    change name to 0.strcat.c

---
+++ 0x06-pointers_arrays_strings/0.strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ *  * _strcat - concatenates two strings
+ *   * @dest: input value
+ *    * @src: input value
+ *     *
+ *      * Return: void
+ *       */
+char *_strcat(char *dest, char *src)
+{
+	int i=0;
+	int j=0;
+	while(dest[i]!="\0")
+	{
+		i++;
+	}
+
+	while(src[j]="\0")
+	{
+		dest[i]=src[j];
+		i++;
+		j++;
+	}
+	dest[j]="\0";
+	return (dest);
+}
