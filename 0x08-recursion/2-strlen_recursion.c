819389b7b6fad93a7d6e80a03f750fd8463796cd
philica <fikretucker79@gmail.com>
2021-07-08 04:29:16 -0700

    create a c file that counts length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,16 +4,15 @@
  * @s: pointer to a string
  * Return: length of the string
  **/
-int i=0;
+int i = 0;
 int _strlen_recursion(char *s)
 {
-	
 	if (*s != '\0')
 	{
 
 		i++;
-		_strlen_recursion(s+1);
+		_strlen_recursion(s + 1);
 
 	}
-	return i;
+	return (i);
 }
