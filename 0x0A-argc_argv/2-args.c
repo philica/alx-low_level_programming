eefa0acf7db4d3c28cd76c2d6f935c84141c3fe8
philica <fikretucker79@gmail.com>
2021-07-12 01:31:49 -0700

    create 0x0A-args_argv dir

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main -  writes its name
+ * main - a program that prints all arguments it receives.
  * @argc: arguement count
  * @argv: array of pointers to arguement strings
  * Return: 0
  **/
 int main(int argc, char *argv[])
 {
-	argc = argc;
-	printf("%s\n", *argv);
+	while (argc--)
+		printf("%s\n", *argv++);
 	return (0);
 }
